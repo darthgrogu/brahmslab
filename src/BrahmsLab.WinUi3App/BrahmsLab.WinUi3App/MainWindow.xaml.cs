ae7a0b6a89cf5cf1feaeeeba5771fd1f9b752d1a
Grogu Melo <melostrader@gmail.com>
2025-07-17 12:57:04 -0400

    feat(shell): Implement first ViewModel and Data Binding

--- src/BrahmsLab.WinUi3App/BrahmsLab.WinUi3App/MainWindow.xaml.cs
+++ src/BrahmsLab.WinUi3App/BrahmsLab.WinUi3App/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+using BrahmsLab.WinUi3App.ViewModels;
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,9 +11,17 @@ namespace BrahmsLab.WinUi3App
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        public MainViewModel ViewModel { get; }
+
         public MainWindow()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+
+            // Instantiate the ViewModel.
+            ViewModel = new MainViewModel();
+
+            // We can set the initial window title from here.
+            this.Title = "BRAHMS Lab";
         }
     }
 }
